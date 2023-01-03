e5e4cb69d327cbb14d72a02615f1fac687248efd
Terje Topland <terjetop@uia.no>
2023-01-03 23:03:22 +0100

    Added start time to Race

---
+++ Src/BlazorApp/Data/Migrations/20230103220036_StartTimeRace.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class StartTimeRace : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartTime",
+                table: "Races",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartTime",
+                table: "Races");
+        }
+    }
+}
--- Src/BlazorApp/Models/Race.cs
+++ Src/BlazorApp/Models/Race.cs
@@ -3,6 +3,8 @@ namespace BlazorApp.Models;
 public class Race
 {
     public int Id { get; set; }
+    
+    public DateTime? StartTime { get; set; } 
 
     public virtual ICollection<Participant> Participants { get; set; } = new HashSet<Participant>();
     

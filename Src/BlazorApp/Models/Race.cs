9da29e4e8958de6ad85edab1c4e772d8815b5afc
Terje Topland <terjetop@uia.no>
2023-01-04 23:51:04 +0100

    Added Races navbar

--- Src/BlazorApp/Models/Race.cs
+++ Src/BlazorApp/Models/Race.cs
@@ -4,6 +4,8 @@ public class Race
 {
     public int Id { get; set; }
     
+    public string Name { get; set; } = String.Empty;
+    
     public DateTime? StartTime { get; set; } 
 
     public virtual ICollection<Participant> Participants { get; set; } = new HashSet<Participant>();

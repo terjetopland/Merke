8e9cb86fdbb5d6868203887448807212c52b7476
Terje Topland <terjetop@uia.no>
2023-01-15 21:40:16 +0100

    Set 'Name' and 'Info' in model 'Race' to nullable

--- Src/BlazorApp/Models/Race.cs
+++ Src/BlazorApp/Models/Race.cs
@@ -3,9 +3,9 @@ namespace BlazorApp.Models;
 public class Race
 {
     public int Id { get; set; }
-    public string Name { get; set; } = string.Empty; // maybe property can be nullable instead?
-    public string Info { get; set; } = string.Empty; // maybe property can be nullable instead?
-    public DateTime? StartTime { get; set; }
+    public string? Name { get; set; }  
+    public string? Info { get; set; }
+    public DateTime? StartTime { get; set; } //Planned time for race to start
     public DateTime? StartRace { get; set; }
     public DateTime? EndRace { get; set; }
     public virtual ICollection<Participant> Participants { get; set; } = new HashSet<Participant>();

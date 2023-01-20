fc694fd93781c5e25d547770b0b03b6d31f4565b
VillereV <skaarbjarte@gmail.com>
2023-01-20 11:51:11 +0100

    Added testproject

--- Src/BlazorApp/Models/Participant.cs
+++ Src/BlazorApp/Models/Participant.cs
@@ -4,7 +4,7 @@ public class Participant
 {
     public int  Id { get; set; }
     public int RaceId { get; set; }
-    public string UserId { get; set; }
+    public string? UserId { get; set; }
     public DateTime? EndTime { get; set; }
     public virtual AppUser? User { get; set; }
     public virtual Race Race { get; set; } = default!;
